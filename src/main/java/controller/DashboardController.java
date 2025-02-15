2570aa51bd4552e57db0e63fa347c91d4b00cba7
Akash Fernando <Sathsarafernando11@gmail.com>
2025-02-15 22:33:38 +0530

    first commit

---
+++ src/main/java/controller/DashboardController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardController {
+
+    @FXML
+    void btnEmployersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSuppliersOnAction(ActionEvent event) {
+
+    }
+
+}
