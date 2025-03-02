af58777ce732cce88730cf84b39e6cd1d3ed9012
Akash Fernando <Sathsarafernando11@gmail.com>
2025-03-02 17:07:23 +0530

    update code

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -2,32 +2,74 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class DashboardController {
 
-    @FXML
-    void btnEmployersOnAction(ActionEvent event) {
+    public AnchorPane ancpane;
+
+   public void btnEmployeeOnAction(ActionEvent actionEvent) {
+
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/addEmployee.fxml"));
+            ancpane.getChildren().clear();
+            ancpane.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    @FXML
-    void btnItemsOnAction(ActionEvent event) {
-
+    public void btnDashboardOnAction(ActionEvent actionEvent) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/dashboardForm.fxml"));
+            ancpane.getChildren().clear();
+            ancpane.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @FXML
-    void btnOrderDetailsOnAction(ActionEvent event) {
-
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/addCustomer.fxml"));
+            ancpane.getChildren().clear();
+            ancpane.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @FXML
-    void btnOrderOnAction(ActionEvent event) {
-
+    public void btnItemOnAction(ActionEvent actionEvent) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/itemForm.fxml"));
+            ancpane.getChildren().clear();
+            ancpane.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @FXML
-    void btnSuppliersOnAction(ActionEvent event) {
-
+    public void btnSupplierOnAction(ActionEvent actionEvent) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/addSupplier.fxml"));
+            ancpane.getChildren().clear();
+            ancpane.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/about.fxml"));
+            ancpane.getChildren().clear();
+            ancpane.getChildren().add(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
