7682766abd219beafa344218e0994b55e7a8f0d3
Akash Fernando <Sathsarafernando11@gmail.com>
2025-07-26 23:34:45 +0530

    place order function

---
+++ src/main/java/PlaceOrderController.java
@@ -0,0 +1,6 @@
+import javafx.event.ActionEvent;
+
+public class PlaceOrderController {
+    public void onPlaceOrder(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/controller/PlaceOrderController.java
+++ src/main/java/controller/PlaceOrderController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class PlaceOrderController {
+    public void CustomerCombo(ActionEvent actionEvent) {
+    }
+
+    public void searchItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void tbltemsOnMouoseReleased(MouseEvent mouseEvent) {
+    }
+
+    public void tblMouseClick(MouseEvent mouseEvent) {
+    }
+
+    public void printBillOnAction(ActionEvent actionEvent) {
+    }
+
+    public void RemoveOnAction(ActionEvent actionEvent) {
+    }
 }
